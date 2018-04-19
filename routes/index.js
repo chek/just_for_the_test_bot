2b5b113416d7f64e22779552dffe6e15ac9b2ff7
chekst <israel.gavriel@gmail.com>
2018-04-19 17:12:53 +0800

    allign eslint

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,16 @@
-var express = require('express');
-var telegram = require("../telegram.js");
-var router = express.Router();
+var express = require('express')
+//var telegram = require('../telegram.js)
+var router = express.Router()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  //telegram.init();
-  var fullUrl = req.protocol + '://' + req.get('host');
-  
-  
-  //var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-  res.render('index', { title: fullUrl });
-  //
-});
+    //telegram.init()
+    var fullUrl = req.protocol + '://' + req.get('host')
+    
+    
+    //var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl
+    res.render('index', { title: fullUrl })
+    //
+})
 
-module.exports = router;
+module.exports = router
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,11 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  console.log('users')
-  //var fullUrl = req.protocol + '://' + req.get('host');
-  res.send(fullUrl);
-});
+    console.log('users')
+    var fullUrl = req.protocol + '://' + req.get('host')
+    res.send(fullUrl)
+})
 
-module.exports = router;
+module.exports = router
